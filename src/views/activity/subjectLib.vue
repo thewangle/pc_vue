d696525c4696a5123d46646d08bbe040f8541d76
王攀 <wangpan04@meituan.com>
2018-10-30 18:04:13 +0800

    build

--- src/views/activity/subjectLib.vue
+++ src/views/activity/subjectLib.vue
@@ -595,8 +595,10 @@ export default {
           )
           if (!res.data.error_code) {
             this.$message({ message: '上传成功', type: 'success' })
-            e.target.value = ''
+          } else {
+            this.$message({ message: res.data.message, type: 'error' })
           }
+          e.target.value = ''
           this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
             loadingInstance.close()
           })
