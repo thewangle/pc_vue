2229cb917239d0b857dbd9f73380a4f160762cf7
lexiang <sdwl05@163.com>
2020-10-13 18:02:50 +0800

    small change

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
+import {
+  MessageBox,
+  Message
+} from 'element-ui'
 import store from '@/store'
 
 // create an axios instance
@@ -34,7 +37,6 @@ service.interceptors.response.use(
    * 以下代码均为样例，请结合自生需求加以修改，若不需要，则可删除
    */
   response => {
-    console.log('22222222')
     const res = response.data
     if (res.error_code !== 0) {
       Message({
